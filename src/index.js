d7a2775b09bdc0e374f83ff0185dba1b5e636fb9
Sandro Soncini <sandrosoncini@hotmail.com>
2020-02-04 10:21:17 -0800

    added demo for questions details

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello from react</h1>, document.getElementById('root'));
+
+// A react component is a function that returns a react element.
+// PascalCase is the naming convention for react components.
+// Components whose names does not begin with a capital letter,
+// will be interpretted as a plain HTML tag.
+const QuestionDetails = () => {
+    return (
+      <div>
+        <h2>What is your favourite color?</h2>
+        <p>
+          Red, green, magenta, etc. <br />
+          By Michael Owen
+        </p>
+        <small>Seen 10 times - Created 10 days ago</small>
+      </div>
+    );
+  };
+
+
+
+
+ReactDOM.render(<QuestionDetails />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
