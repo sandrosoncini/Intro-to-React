408efe7c08ea475a28ed3a2ef026f1d31379936e
Sandro Soncini <sandrosoncini@hotmail.com>
2020-02-04 10:37:59 -0800

    added question and aswers together

--- src/index.js
+++ src/index.js
@@ -36,10 +36,21 @@ const AnswerDetails = () =>{
 
         </div>
     )
-}
+};
+
+// question Show page component 
+const QuestionShowPage =()=>{
+    return (
+        <div>
+        <QuestionDetails/>
+        <AnswerDetails/>
+        
+        </div>
+    )
+};
 
 
-ReactDOM.render(<AnswerDetails  />, document.getElementById('root'));
+ReactDOM.render(<QuestionShowPage  />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
